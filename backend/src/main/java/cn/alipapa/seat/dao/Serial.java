a42a7f133cb4b6a72a549c324375d657b92ea971
MiloWang <milowang2048@foxmail.com>
2020-05-30 22:05:27 +0800

    后端：添加Serial类到DAO层

---
+++ backend/src/main/java/cn/alipapa/seat/dao/Serial.java
@@ -0,0 +1,32 @@
+package cn.alipapa.seat.dao;
+
+public class Serial {
+
+    // 所有属性均为private
+
+    private String serial;
+    private String password;
+
+    // 每个属性均有对应的getter/setter
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Serial(String serial, String password) {
+        this.serial = serial;
+        this.password = password;
+    }
+}
