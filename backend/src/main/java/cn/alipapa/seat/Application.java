ea521065650e88665ebc4d378522358488fe8fb4
MiloWang <milowang2048@foxmail.com>
2020-06-11 13:28:08 +0800

    后端：修改主类名为Application

--- backend/src/main/java/cn/alipapa/seat/Application.java
+++ backend/src/main/java/cn/alipapa/seat/Application.java
@@ -1,11 +1,13 @@
 package cn.alipapa.seat;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+@MapperScan("cn.alipapa.seat.dao")
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
