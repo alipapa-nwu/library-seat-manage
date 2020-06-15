2480ca8411667d4dfdd849b9abbdab2d6523c0bd
MiloWang <milowang2048@foxmail.com>
2020-06-15 19:28:58 +0800

    后端：修正返回Json切面，现在只切入controller层 只会影响execution(public Object cn.alipapa.seat.service..*(*))的方法

--- backend/src/main/java/cn/alipapa/seat/aspect/ReturnJsonAspect.java
+++ backend/src/main/java/cn/alipapa/seat/aspect/ReturnJsonAspect.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class ReturnJsonAspect {
-    @Pointcut("execution(public Object cn.alipapa.seat.service..*(*))")
+    @Pointcut("execution(public Object cn.alipapa.seat.controller..*(*))")
     public void pointcut() {
     }
 
@@ -18,16 +18,16 @@ public class ReturnJsonAspect {
     public Object aroundBeanToString(ProceedingJoinPoint joinPoint) throws Throwable {
         // 正常执行方法，获取返回值
         var args = joinPoint.getArgs();
-        var res = joinPoint.proceed(args);
+        var result = joinPoint.proceed(args);
         // 如果res为空，抛异常
-        if (res == null)
-            throw new NullPointerException("null return value of service " + joinPoint.getThis().getClass().getName());
+        if (result == null)
+            throw new NullPointerException("null return value of controller " + joinPoint.getThis().getClass().getName());
         // 创建一个返回结构
         var returnJson = new HttpResponseBody();
         // 状态码200
         returnJson.setStatus(200);
         // 设置原方法返回值为返回json的data字段
-        returnJson.setData(res);
+        returnJson.setData(result);
         // 替换返回值
         return returnJson;
     }
