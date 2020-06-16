7d860c9fd585acfc6f32e4f03c307e705d338a10
MiloWang <milowang2048@foxmail.com>
2020-06-16 20:37:35 +0800

    前端：用假数据渲染level-button到主页

--- frontend/pages/index/index.js
+++ frontend/pages/index/index.js
@@ -1,7 +1,31 @@
 // index.js
-
 Page({
   data: {
-    
+    levelData: [
+      {
+        level: 1,
+        remain: 60
+      },
+      {
+        level: 2,
+        remain: 50
+      },
+      {
+        level: 3,
+        remain: 40
+      },
+      {
+        level: 4,
+        remain: 30
+      },
+      {
+        level: 5,
+        remain: 20
+      },
+      {
+        level: 6,
+        remain: 10
+      },
+    ]
   }
 })
