19a52102301200219c31925bbf6530d26b3c0a95
MooYi029 <60977686+MooYi029@users.noreply.github.com>
2020-06-17 17:26:18 +0800

    mine-status-button

---
+++ frontend/components/status-button/status-button.js
@@ -0,0 +1,20 @@
+// components/finished-button/finished-button.js
+Component({
+  properties: {
+    status:String,
+    color:String,
+    image:String,
+    date:String,
+  },
+
+  data: {
+   // status:'已完成',
+    //date:'2020.06.02 8:00-10:00',
+    //color:'green',
+    //myimage:'/assets/complete.png',
+  },
+
+  methods: {
+
+  }
+})
