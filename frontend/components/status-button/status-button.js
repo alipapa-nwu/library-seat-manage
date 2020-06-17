836caefec9754237952c2b635557627d83ac538f
MiloWang <milowang2048@foxmail.com>
2020-06-17 23:39:15 +0800

    前端：改进status-button

--- frontend/components/status-button/status-button.js
+++ frontend/components/status-button/status-button.js
@@ -1,20 +1,39 @@
 // components/finished-button/finished-button.js
 Component({
   properties: {
-    status:String,
-    color:String,
-    image:String,
-    date:String,
+    status: String,
+    description: String
   },
-
   data: {
-   // status:'已完成',
-    //date:'2020.06.02 8:00-10:00',
-    //color:'green',
-    //myimage:'/assets/complete.png',
+    color: "",
+    title: "",
+    subtitle: "",
+    imgPath: ""
   },
-
-  methods: {
-
+  lifetimes: {
+    attached() {
+      const {status, description} = this.properties;
+      const stateMap = {
+        success: {
+          title: "已完成",
+          color: "green",
+          subtitle: description,
+          imgPath: "/assets/reservation_success.png"
+        },
+        pending: {
+          title: "待完成",
+          color: "blue",
+          subtitle: description,
+          imgPath: "/assets/reservation_pending.png"
+        },
+        fail: {
+          title: "未完成",
+          color: "red",
+          subtitle: description,
+          imgPath: "/assets/reservation_fail.png"
+        }
+      };
+      this.setData(stateMap[status]);
+    }
   }
 })
--- frontend/pages/mine/mine.js
+++ frontend/pages/mine/mine.js
@@ -1,71 +1,19 @@
 // pages/mine/mine.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    mineDate:[
-      '2020.06.05 08:00-10:00',
-      '2020.06.01 10:00-12:00',
-      '2020.05.30 14:00-16:00',
+    reservations: [
+      {
+        status: "success",
+        description: "2020.06.05 08:00-10:00"
+      },
+      {
+        status: "pending",
+        description: "2020.06.05 08:00-10:00"
+      },
+      {
+        status: "fail",
+        description: "2020.06.05 08:00-10:00"
+      },
     ]
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
 })
