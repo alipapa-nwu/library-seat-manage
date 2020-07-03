d54301da926e50240e215d2185256c4090b69953
MiloWang <milowang2048@foxmail.com>
2020-07-03 12:05:46 +0800

    前端：添加启动逻辑

--- frontend/app.js
+++ frontend/app.js
@@ -2,7 +2,17 @@
 
 App({
   onLaunch: function () {
-    
+    wx.getStorage({
+      key: 'userInfo',
+      success: res=>{
+        this.globalData.userInfo = res.data;
+      }
+    })
+    wx.getSystemInfo({
+      complete: (res) => {
+        this.globalData.platform = res.platform;
+      },
+    })
   },
   globalData: {}
 })
