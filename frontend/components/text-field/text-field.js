4b673d993af5e6410bf909c237f2fd24ba12a339
MiloWang <milowang2048@foxmail.com>
2020-07-03 12:07:46 +0800

    前端：为输入框添加额外的属性

--- frontend/components/text-field/text-field.js
+++ frontend/components/text-field/text-field.js
@@ -1,32 +1,10 @@
 // components/text-field/text-field.js
 Component({
-  /**
-   * 组件的属性列表
-   */
   properties: {
-    placeholder: {
-      type: String,
-      default: ""
-    },
-    label: {
-      type: String,
-      default: ""
-    }
-  },
-
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-    onInput(e){
-      this.triggerEvent('change', e.detail);
-    },
+    placeholder: String,
+    label: String,
+    type: String,
+    value: String,
+    password: Boolean
   }
 })
