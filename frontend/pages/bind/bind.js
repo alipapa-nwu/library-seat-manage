88fdc946b5be7c930054260e66f9ac01568f817c
MiloWang <milowang2048@foxmail.com>
2020-07-03 12:08:05 +0800

    前端：添加绑定逻辑

--- frontend/pages/bind/bind.js
+++ frontend/pages/bind/bind.js
@@ -1,11 +1,50 @@
 // pages/bind/bind.js
+const app = getApp();
+
 Page({
   data: {
     serial: "",
     password: ""
   },
   onSubmmit(e) {
-    console.log(this.data)
+    wx.showLoading({
+      title: '正在绑定',
+    });
+    wx.request({
+      url: 'https://seat.milolab.cn/private/bind_serial',
+      method: 'POST',
+      data: {
+        serial: this.data.serial,
+        password: this.data.password
+      },
+      header: {
+        'open-id': app.globalData.userInfo.openId,
+        'session-id': app.globalData.userInfo.sessionId
+      },
+      success: res=>{
+        app.globalData.userInfo = {
+          ...app.globalData.userInfo,
+          serial: this.data.serial
+        }
+        wx.setStorage({
+          data: app.globalData.userInfo,
+          key: 'userInfo',
+        })
+        this.getOpenerEventChannel().emit('updateSerial', {
+          serial: this.data.serial
+        })
+        wx.navigateBack();
+      },
+      fail: res=>{
+        wx.showToast({
+          title: '绑定失败',
+          image: '/assets/error.png'
+        });
+      },
+      complete: res=>{
+        wx.hideLoading();
+      }
+    })
   },
   onChange(e){
     const data = {};
